e0fa6d56246dd5343a0b5c23bcdffaac2eeb04c2
Rajan kumar <krcpr007@gmail.com>
2021-10-28 23:32:35 +0530

    button chnged

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './About.css';
-import {FaFacebook} from 'react-icons/fa'
 import myImg from './myPic.jpg'
 const About = () => {
     return (
@@ -10,7 +9,7 @@ const About = () => {
                     <h1 className="about-heading"><span style={{ color: "#64ffda" }}>01.</span> About Me</h1>
                 </div>
                <div className="myImg">
-                   <img src={myImg} alt="" />
+                   <img className="img" src={myImg} alt="" />
                </div> 
                <div className="about-pragraph">
                 <p>Hello! My name is Rajan and I enjoy creating things that live on <br/>the internet.My interest in web development started back in 2019 <br/> when I decided to try editing custom Tumblr themes — turns out  <br/>hacking together a custom reblog button taught me a lot  <br/>about HTML & CSS!<br/>
