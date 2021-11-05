7ff9f3e6d4d5158eb06191cad3221261aeff1a77
Rajan kumar <krcpr007@gmail.com>
2021-11-05 17:12:02 +0530

    some projects added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-// import {
-//   BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
 //   Switch,
 //   Route
-// } from "react-router-dom";
+} from "react-router-dom";
+import { useEffect, useState } from 'react';
 import Projects from './component/Project/Projects'
 import About from './component/About/About';
 import Navbar from './component/NavBar/Navbar';
 import Home from './component/Home/Home';
-import Footer from './component/Footer/Footer'
+import Footer from './component/Footer/Footer';
+import './App.css'
+import Loder from './component/Loder/Loder';
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect ( ()=>{
+        setloading(true); 
+        setTimeout(() => {
+          setloading(false);
+        }, 4000);
+  },[]);
   return (
-    <>
-      {/* <Router> */}
-        <Navbar />
-        {/* <Switch> */}
-          {/* <Route exact path="/"> */}
-          <Home/>
-          {/* </Route> */}
-          {/* <Route exact path="/about"> */}
-            <About />
-          {/* </Route> */}
-          {/* <Route exact path="/projects"> */}
-            <Projects/>
-          {/* </Route> */}
-        {/* </Switch> */}
-        <Footer/>
-      {/* </Router> */}
-    </>
+      <div> 
+    { loading ? (
+    <Loder/>
+    )
+      :(
+        <>
+              <Router>
+               <Navbar />
+              {/* <Switch> */}
+              {/* <Route exact path="/"> */}
+                {/* <preloader/> */}
+              <Home />
+              {/* </Route> */}
+              {/* <Route exact path="/about"> */}
+              <About id="#about" />
+              {/* </Route> */}
+              {/* <Route exact path="/projects"> */}
+              <Projects />
+              {/* </Route> */}
+              {/* </Switch> */}
+              <Footer />
+              </Router>
+              </>
+      )}
+      </div>
   );
 }
 
