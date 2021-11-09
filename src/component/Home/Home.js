960eae48d0bb8d2cacd0b45ffaf7032b516d28b1
Rajan kumar <krcpr007@gmail.com>
2021-11-09 17:14:57 +0530

    pic added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,13 +4,12 @@ import { FaGithubAlt } from 'react-icons/fa';
 const Home = () => {
     return (
         <>
-            {/* <img className="" src={myImg} alt="MY IMAGE" /> */}
             <div data-aos="fade-left" className="home-container">
                 <div className="home-container2">
                     <p className="home-paragraph">Hii, my name is </p>
                     <h1 className="home-heading1">Rajan Kumar</h1>
                     <h1 className="home-heading2">I build things for the web</h1>
-                    <p className="home-paragraph-about" >I’m a software engineer specializing in building (and occasionally<br /> designing) exceptional digital experiences. Currently, I’m focused on my B.tech at <a style={{ color: "#64ffda" }} href="http://www.nitp.ac.in/php/home.php" target="_blank" rel="noreferrer">Nit patna</a></p>
+                    <p className="home-paragraph-about"  >I’m a software engineer specializing in building (and occasionally<br /> designing) exceptional digital experiences. Currently, I’m focused on my B.tech at <a style={{ color: "#64ffda" }} href="http://www.nitp.ac.in/php/home.php" target="_blank" rel="noreferrer">Nit patna</a></p>
                     <div>
                     <form target="_blank" action="https://github.com/krcpr007">
 
